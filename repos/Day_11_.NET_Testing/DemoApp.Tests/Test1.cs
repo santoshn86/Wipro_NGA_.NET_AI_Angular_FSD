fd91c9032ef7e78b901499296b714c3c582989ad
santosh-devQA86 <santoshdn9220@gmail.com>
2026-04-27 18:04:49 +0530

    Day 11's session code

---
+++ repos/Day_11_.NET_Testing/DemoApp.Tests/Test1.cs
@@ -0,0 +1,21 @@
+﻿namespace DemoApp.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        //Arrange
+        var calculator = new Calcualtor();
+        int a = 5;
+        int b = 10;
+        int expected = 50;
+
+        //Act
+        var result = calculator.Multiply(a, b);
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ repos/Day_11_.NET_Testing/Test_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, here we are impleting TDD using .NEt Core !");
+//business logic
+public class Calcualtor
+{
+    public int Multiply( int a, int b)
+    {
+        return a*b;
+    }
+}
